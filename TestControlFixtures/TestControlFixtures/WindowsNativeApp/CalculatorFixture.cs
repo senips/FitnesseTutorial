3ac0a815fae83cf938d1b9ea78994f155a9f2fef
Senthil Maruthaiappan <senips@gmail.com>
2015-01-11 22:51:34 -0500

    calculator tests
    calculator tests

--- TestControlFixtures/TestControlFixtures/WindowsNativeApp/CalculatorFixture.cs
+++ TestControlFixtures/TestControlFixtures/WindowsNativeApp/CalculatorFixture.cs
@@ -15,28 +15,69 @@ namespace TestControlFixtures.WindowsNativeApp
 {
     public class CalculatorFixture
     {
-
         public CalculatorFixture()
         {
+            //create all digit locators
+            CreateDigitButtons();
+            //create result display
+            var locator = new ControlLocatorDefRepo("result");
+            locator.FindUsing("app");
+            locator.FindByAutomationId("403");
+
+            //locator for =
+            CreateLocatorByName("=");
             
         }
 
-        public void ClickTheButton()
-        {
-            GetButton().Click();
-        }        
-
-        private ButtonControl GetButton()
+        public void EnterValue(String aValue)
         {
             var button = new ButtonControl();
-            button.SystemUnderTest(new ControlLocatorDef<FindControl>(
-                                                () => new FindWindow(""),
-                                                () => new FindByAutomationId("buttonOk")
-                    ));
-            return button;
+            foreach (char ch in aValue)
+            {
+                button.SystemUnderTestFromRepo(ch.ToString());
+                button.Click();
+                button.Wait(0, 50);
+            }
+        }        
+
+        private void CreateDigitButtons()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                CreateLocatorByName(i.ToString());
+            }             
         }
 
-    
+        private void CreateLocatorByName(String name)
+        {
+            var locator = new ControlLocatorDefRepo(name);
+            locator.FindUsing("app");
+            locator.FindByName(name);
+        }
 
+        public void Operation(String opr)
+        {
+            CreateLocatorByName(opr);
+            var button = new ButtonControl();            
+            button.SystemUnderTestFromRepo(opr);
+            button.Wait(0, 50);
+            button.Click();
+        }
+
+        public String Result
+        {
+            get
+            {
+               
+                var button = new ButtonControl();
+                button.SystemUnderTestFromRepo("=");
+                button.Wait(0, 20);
+                button.Click();
+
+                var txt = new TextBoxControl();
+                txt.SystemUnderTestFromRepo("result");
+                return txt.Text.Trim();
+            }
+        } 
     }
 }
